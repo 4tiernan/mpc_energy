433afa3a98448f9a164df7ac24f73d677734bd54
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-03 19:02:21 +1000

    fixed init wrong const import and voluptuous setup inputs, setup working now

--- custom_components/mpc_energy/__init__.py
+++ custom_components/mpc_energy/__init__.py
@@ -1,7 +1,10 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-from .const import DOMAIN
+from .const import (
+    DOMAIN,
+    DEFAULT_NAME
+)
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up MPC Energy from YAML (not used)."""
@@ -15,10 +18,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return True
 
-from const import (
-    DOMAIN,
-    DEFAULT_NAME
-)
+
 
 ##### LOGGGING !!!!!!!
 
--- custom_components/mpc_energy/config_flow.py
+++ custom_components/mpc_energy/config_flow.py
@@ -1,22 +1,29 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import selector
 from .const import DOMAIN
 
 class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for MPC Energy."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="MPC Energy", data=user_input)
+            return self.async_create_entry(
+                title="MPC Energy",
+                data=user_input,
+            )
 
-        schema = vol.Schema({
-            vol.Required("battery_entity"): cv.entity_id,
-            vol.Required("price_entity"): cv.entity_id,
-            vol.Optional("solar_entity"): cv.entity_id,
-        })
-        return self.async_show_form(step_id="user", data_schema=schema)
+        schema = vol.Schema(
+            {
+                vol.Required("battery_entity"): selector.EntitySelector(),
+                vol.Required("price_entity"): selector.EntitySelector(),
+                vol.Optional("solar_entity"): selector.EntitySelector(),
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+        )

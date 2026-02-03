a4dbd52bd789ef2faa4e51065e6eca3a96a28f61
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-04 07:33:45 +1000

    Working on config flow inputs

--- custom_components/mpc_energy/config_flow.py
+++ custom_components/mpc_energy/config_flow.py
@@ -3,6 +3,36 @@ from homeassistant import config_entries
 from homeassistant.helpers import selector
 from .const import DOMAIN
 
+schema = vol.Schema( # Define all inputs to the integration.
+    {
+        vol.Required("Battery Discharge Cost ($/kWh)"): selector.NumberSelector(selector.NumberSelectorConfig(
+                min=0,
+                max=1,
+                step=0.001,
+                unit_of_measurement="$/kWh",
+                mode=selector.NumberSelectorMode.BOX,
+            )),
+
+        vol.Required("Battery Rated Capacity"): selector.EntitySelector(),
+        vol.Optional("Backup Buffer SOC"): selector.EntitySelector(),
+        vol.Required("Charge Cut-Off SOC"): selector.EntitySelector(),
+        vol.Required("Battery Charge Power Limit"): selector.EntitySelector(),
+        vol.Required("Battery Discharge Power Limit"): selector.EntitySelector(),
+        vol.Required("Battery SOC"): selector.EntitySelector(),
+
+        vol.Required("Solar MPPT DC Power Limit"): selector.EntitySelector(),
+        vol.Required("Inverter AC Power Limit"): selector.EntitySelector(),
+        vol.Required("Grid Import Power Limit"): selector.EntitySelector(),
+        vol.Required("Grid Export Power Limit"): selector.EntitySelector(),
+
+
+        vol.Required("price_entity"): selector.EntitySelector(),
+        vol.Optional("solar_entity"): selector.EntitySelector(),
+        
+    }
+)
+
+
 class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for MPC Energy."""
 
@@ -25,5 +55,19 @@ class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
+            data_schema=schema,
+        )
+        
+    async def async_step_reconfigure(self, user_input=None):
+        if user_input is not None:
+            return self.async_update_reload_and_abort(
+                self._get_reconfigure_entry(),
+                data_updates=user_input,
+            )
+        
+        
+
+        return self.async_show_form(
+            step_id="reconfigure",
             data_schema=schema,
         )

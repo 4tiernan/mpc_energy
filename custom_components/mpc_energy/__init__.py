a57fdef304e40bc847edb41380dfd64f638ed919
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-04 15:11:21 +1000

    Commenting and removed sensor.py entity category and added entity measurement class

--- custom_components/mpc_energy/__init__.py
+++ custom_components/mpc_energy/__init__.py
@@ -7,7 +7,7 @@ async def async_setup_entry(hass, entry):
     # Initial refresh
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator # Define coordinator for integration
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
--- custom_components/mpc_energy/sensor.py
+++ custom_components/mpc_energy/sensor.py
@@ -1,11 +1,12 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.helpers.entity import EntityCategory
+
 from .const import DOMAIN
 
+# Setup the entity in HA
 async def async_setup_entry(hass, entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([EffectivePriceSensor(coordinator)])
+    coordinator = hass.data[DOMAIN][entry.entry_id] # Retrieve coordinator instance defined in __init__.py
+    async_add_entities([EffectivePriceSensor(coordinator)]) # Creates the entity in HA
 
 
 class EffectivePriceSensor(CoordinatorEntity, SensorEntity):
@@ -13,11 +14,12 @@ class EffectivePriceSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_name = "Effective Price"
         self._attr_unique_id = "mpc_energy_effective_price"
-        self._attr_unit_of_measurement = "W"
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_unit_of_measurement = "$/kWh"
+        self._attr_state_class = "measurement"  # key to history graph
+        #self._attr_device_class = "power"
 
     @property
-    def native_value(self):
+    def native_value(self): # Method to return the state to HA when it needs it
         if self.coordinator.data is None:
             return None
-        return self.coordinator.data["effective_price"]
+        return self.coordinator.data["effective_price"] # Pull the state directly from the coordinator

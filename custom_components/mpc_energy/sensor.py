2b1dc1102f3f7c4a519588960ae27db97aca45d3
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-04 14:00:33 +1000

    Fixed Coordinator Entity in sensor.py not following HA standards. Base functionality now implemented

--- custom_components/mpc_energy/sensor.py
+++ custom_components/mpc_energy/sensor.py
@@ -1,4 +1,5 @@
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinatorEntity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.entity import EntityCategory
 from .const import DOMAIN
 
@@ -7,12 +8,12 @@ async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities([EffectivePriceSensor(coordinator)])
 
 
-class EffectivePriceSensor(DataUpdateCoordinatorEntity):
+class EffectivePriceSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator):
         super().__init__(coordinator)
         self._attr_name = "Effective Price"
         self._attr_unique_id = "mpc_energy_effective_price"
-        self._attr_unit_of_measurement = "$/kWh" 
+        self._attr_unit_of_measurement = "W"
         self._attr_entity_category = EntityCategory.DIAGNOSTIC
 
     @property

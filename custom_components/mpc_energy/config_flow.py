ab58af79a74232593969717da244c6a3044d5854
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-04 15:44:50 +1000

    Auto populate reconfigure form with known values

--- custom_components/mpc_energy/config_flow.py
+++ custom_components/mpc_energy/config_flow.py
@@ -4,53 +4,57 @@ from homeassistant.helpers import selector
 from .const import DOMAIN, PlantEntites
 
 
+def create_schema(entry=None):
+    get_default = lambda key, fallback=None: entry.data.get(key, fallback) if entry else fallback
 
-schema = vol.Schema( # Define all inputs to the integration.
-    {
-        vol.Required(PlantEntites.BATTERY_DISCHARGE_COST): selector.NumberSelector(selector.NumberSelectorConfig(
-                min=0,
-                max=1,
-                step=0.001,
-                unit_of_measurement="$/kWh",
-                mode=selector.NumberSelectorMode.BOX,
+    schema = vol.Schema( # Define all inputs to the integration.
+        {
+            
+            vol.Required(PlantEntites.BATTERY_DISCHARGE_COST, default=get_default(PlantEntites.BATTERY_DISCHARGE_COST, 0)): selector.NumberSelector(selector.NumberSelectorConfig(
+                    min=0,
+                    max=1,
+                    step=0.001,
+                    unit_of_measurement="$/kWh",
+                    mode=selector.NumberSelectorMode.BOX,
             )),
-
-        vol.Required(PlantEntites.HA_EMS_CONTROL_SWITCH): selector.EntitySelector(),
-        vol.Required(PlantEntites.EMS_CONTROL_MODE): selector.EntitySelector(),
-        vol.Required(PlantEntites.DISCHARGE_LIMITER): selector.EntitySelector(),
-        vol.Required(PlantEntites.CHARGE_LIMITER): selector.EntitySelector(),
-        vol.Required(PlantEntites.PV_LIMTER): selector.EntitySelector(),
-        vol.Required(PlantEntites.EXPORT_LIMITER): selector.EntitySelector(),
-        vol.Required(PlantEntites.IMPORT_LIMTER): selector.EntitySelector(),
-
-
-        vol.Required(PlantEntites.BATTERY_RATED_CAPACITY): selector.EntitySelector(),
-        vol.Optional(PlantEntites.BACKUP_SOC): selector.EntitySelector(),
-        vol.Required(PlantEntites.CHARGE_CUTOFF_SOC): selector.EntitySelector(),
-        
-        vol.Required(PlantEntites.BATTERY_SOC): selector.EntitySelector(),
-
-        vol.Required(PlantEntites.CHARGE_LIMIT): selector.EntitySelector(),
-        vol.Required(PlantEntites.DISCHARGE_LIMIT): selector.EntitySelector(),
-        vol.Required(PlantEntites.PV_LIMIT): selector.EntitySelector(),
-        vol.Required(PlantEntites.INVERTER_LIMIT): selector.EntitySelector(),
-        vol.Required(PlantEntites.IMPORT_LIMIT): selector.EntitySelector(),
-        vol.Required(PlantEntites.EXPORT_LIMIT): selector.EntitySelector(),
-
-        vol.Required(PlantEntites.LOAD_POWER): selector.EntitySelector(),
-        vol.Required(PlantEntites.SOLAR_POWER): selector.EntitySelector(),
-        vol.Required(PlantEntites.BATTERY_POWER): selector.EntitySelector(),
-        vol.Required(PlantEntites.INVERTER_POWER): selector.EntitySelector(),
-        vol.Required(PlantEntites.GRID_POWER): selector.EntitySelector(),
-        
-
-        vol.Required(PlantEntites.SOLCAST_FORECAST): selector.EntitySelector(),
+            
+            #vol.Required(PlantEntites.HA_EMS_CONTROL_SWITCH): selector.EntitySelector(),
+            #vol.Required(PlantEntites.EMS_CONTROL_MODE): selector.EntitySelector(),
+            #vol.Required(PlantEntites.DISCHARGE_LIMITER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.CHARGE_LIMITER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.PV_LIMTER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.EXPORT_LIMITER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.IMPORT_LIMTER): selector.EntitySelector(),
 
 
-        #vol.Required("price_entity"): selector.EntitySelector(),
-        #vol.Optional("solar_entity"): selector.EntitySelector(),
-    }
-)
+            #vol.Required(PlantEntites.BATTERY_RATED_CAPACITY): selector.EntitySelector(),
+            #vol.Optional(PlantEntites.BACKUP_SOC): selector.EntitySelector(),
+            #vol.Required(PlantEntites.CHARGE_CUTOFF_SOC): selector.EntitySelector(),
+            
+            #vol.Required(PlantEntites.BATTERY_SOC): selector.EntitySelector(),
+
+            #vol.Required(PlantEntites.CHARGE_LIMIT): selector.EntitySelector(),
+            #vol.Required(PlantEntites.DISCHARGE_LIMIT): selector.EntitySelector(),
+            #vol.Required(PlantEntites.PV_LIMIT): selector.EntitySelector(),
+            #vol.Required(PlantEntites.INVERTER_LIMIT): selector.EntitySelector(),
+            #vol.Required(PlantEntites.IMPORT_LIMIT): selector.EntitySelector(),
+            #vol.Required(PlantEntites.EXPORT_LIMIT): selector.EntitySelector(),
+
+            #vol.Required(PlantEntites.LOAD_POWER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.SOLAR_POWER): selector.EntitySelector(),
+            vol.Required(PlantEntites.BATTERY_POWER, default=get_default(PlantEntites.BATTERY_POWER)): selector.EntitySelector(),
+            #vol.Required(PlantEntites.INVERTER_POWER): selector.EntitySelector(),
+            #vol.Required(PlantEntites.GRID_POWER): selector.EntitySelector(),
+            
+
+            #vol.Required(PlantEntites.SOLCAST_FORECAST): selector.EntitySelector(),
+
+
+            #vol.Required("price_entity"): selector.EntitySelector(),
+            #vol.Optional("solar_entity"): selector.EntitySelector(),
+        }
+    )
+    return schema
 
 
 class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -64,6 +68,8 @@ class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 title="MPC Energy",
                 data=user_input,
             )
+        
+        schema = create_schema()
 
         return self.async_show_form(
             step_id="user",
@@ -71,13 +77,15 @@ class MPCEnergyFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         )
         
     async def async_step_reconfigure(self, user_input=None):
+        entry = self._get_reconfigure_entry()  # get the existing config entry
+
         if user_input is not None:
             return self.async_update_reload_and_abort(
                 self._get_reconfigure_entry(),
                 data_updates=user_input,
             )
         
-        
+        schema = create_schema(entry)
 
         return self.async_show_form(
             step_id="reconfigure",

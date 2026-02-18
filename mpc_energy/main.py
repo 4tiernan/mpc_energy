28cc385837ca28196d93609c5be3c50bcfb1e012
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-02-18 10:23:56 +1000

    Removed log level selector

--- mpc_energy/main.py
+++ mpc_energy/main.py
@@ -28,18 +28,7 @@ handler.setFormatter(formatter)
 # Set up the logger
 logger = colorlog.getLogger()
 logger.addHandler(handler)
-
-configured_log_level = config_manager.log_level  # e.g. "debug"
-
-LOG_LEVELS = {
-    "debug": logging.DEBUG,
-    "info": logging.INFO,
-    "warning": logging.WARNING,
-    "error": logging.ERROR,
-    "critical": logging.CRITICAL,
-}
-
-logger.setLevel(LOG_LEVELS.get(configured_log_level.lower(), logging.INFO))
+logger.setLevel(logging.INFO)
 
 
 # Configure logging with timestamps without milliseconds

3e33e41060b01b150d7585f32dee8d0eb686c6c4
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-03-07 21:17:06 +1000

    Improved logging for disabled entites, and added check that required entites are enabled. Bumped Version

--- mpc_energy/ha_api.py
+++ mpc_energy/ha_api.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
 import time
 from mpc_logger import logger
+import config_manager
+
 
 
 @dataclass
@@ -40,14 +42,18 @@ class HomeAssistantAPI:
 
     def check_api_running(self): #Checks to see if we can connect to the ha api
         url = f"{self.base_url}/api/"
-        r = requests.get(url, headers=self.headers, params=None)
-        #r.raise_for_status()
-        response = r.json()
+        try:
+            r = requests.get(url, headers=self.headers, params=None, timeout=5)
+            response = r.json()
+        except:
+            return False
+        
         return response.get("message") == "API running."
 
     def ha_request(self, url, method, data=None, params = None, headers = None):
-        def raise_for_status():
-            if(r.status_code == 401):
+        def log_status(r):
+            status_code = r.status_code
+            if(status_code == 401):
                 logger.error("Unauthorized when connecting to HA API. Please check your token and ensure it has the necessary permissions.")
                 exit()
 
@@ -55,13 +61,13 @@ class HomeAssistantAPI:
             headers = self.headers
         try:
             if(method =='get'):
-                r = requests.get(url, headers=headers, params=params)
-                raise_for_status()
+                r = requests.get(url, headers=headers, params=params, timeout=5)
+                log_status(r)
                 r.raise_for_status()
                 return r.json()
             elif(method == 'post'):
-                r = requests.post(url, json=data, headers=headers)
-                raise_for_status()
+                r = requests.post(url, json=data, headers=headers, timeout=5)
+                log_status()
                 r.raise_for_status()
                 return r.json()
             else: 
@@ -69,7 +75,8 @@ class HomeAssistantAPI:
         except Exception as e:
             if(self.check_api_running()):
                 entity_id = url.split("/api/states/")[-1]
-                logger.error(f"Able to connect to HA API but the entity {entity_id} was not found. Is it disabled?")
+                logger.error(f"Able to connect to HA API but the entity '{entity_id}' was not found. Is it disabled?")
+                exit()
             else:
                 while(not self.check_api_running()): # If we can't connect to the HA API, wait and retry until we can. This is to handle the case where the add-on starts before HA is fully up and running.
                     logger.error(f"Unable to connect to HA API, retrying in 30 seconds")
@@ -84,7 +91,11 @@ class HomeAssistantAPI:
     
     def get_numeric_state(self, entity_id):
         json_resp = self.get_state(entity_id)
-        return float(json_resp["state"])
+        if not json_resp:
+            logger.error(f"No response returned for '{entity_id}'. Please check if the entity_id is correct")
+            exit()
+        else:
+            return float(json_resp["state"])
 
     def call_service(self, domain, service, data):
         url = f"{self.base_url}/api/services/{domain}/{service}"
@@ -176,3 +187,40 @@ class HomeAssistantAPI:
         data = data or {}
         url = f"{self.base_url}/api/events/{event_type}"
         return self.ha_request(url=url, method='post', data=data)
+    
+
+    def check_required_entities(self):
+        entity_ids = [
+            config_manager.ha_ems_control_switch_entity_id,
+            config_manager.ems_control_mode_entity_id,
+            config_manager.backup_soc_entity_id,
+            config_manager.charge_cutoff_soc_entity_id,
+            config_manager.battery_max_charge_power_limit_entity_id,
+            config_manager.battery_max_discharge_power_limit_entity_id,
+            config_manager.export_max_power_limit_entity_id,
+            config_manager.import_max_power_limit_entity_id,
+            config_manager.pv_max_power_limit_entity_id,
+            config_manager.battery_kwh_till_full_entity_id,
+            config_manager.battery_max_charge_power_limit_entity_id,
+            config_manager.battery_max_discharge_power_limit_entity_id
+        ]
+
+        missing_entities = []
+        for entity_id in entity_ids:
+            try:
+                self.get_state(entity_id)
+            except Exception as e:
+                missing_entities.append(entity_id)
+        
+        if missing_entities:
+            logger.error(f"The following required entities are missing or unavailable: {', '.join(missing_entities)}. Please check if they exist and are enabled.")
+            exit()
+
+
+ha = HomeAssistantAPI(
+    base_url="http://192.168.0.130:8123",
+    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI4ZGU5YzI0NGEyNmE0ZjlmYWU2YTM3Njg2OGI5NDQwMyIsImlhdCI6MTc3Mjg0NTQzNSwiZXhwIjoyMDg4MjA1NDM1fQ.t5MCbnPDkwlgXtVk962lMCFcdNJ6Bcce91WEjipea6g",
+    errors=True
+)
+#ha.check_required_entities()
+#print(ha.get_numeric_state("number.sigen_plant_ess_backup_state_of_charge"))

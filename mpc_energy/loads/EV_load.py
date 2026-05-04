ce13fceb93c299039afa9c56fe984561752dbdcb
4tiernan <41096273+4tiernan@users.noreply.github.com>
2026-05-04 21:17:45 +1000

    debug

--- mpc_energy/loads/EV_load.py
+++ mpc_energy/loads/EV_load.py
@@ -54,6 +54,7 @@ class EVLoad(OptionalLoad):
             start, end = data_helpers.get_time_range_from_hours(hours, self.local_tz)
 
         history = self.ha.get_history(self.power_entity_id, start_time=start, end_time=end)
+        logger.debug(f"Retrieved history for EV load {self.name}: {history}")
         if not history: return None
         
         requested_seconds = max((end - start).total_seconds(), 1.0)
